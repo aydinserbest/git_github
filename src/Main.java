3dd2235eb770cba2e054275274b438f9fcfb928f
aydinSerbest <aydinserbest34@gmail.com>
2024-03-06 21:24:00 +0100

    updated git1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello world!");
       System.out.println("Hello you too!");
         System.out.println("Hello Mars!");
-        System.out.println("hi");
+        System.out.println("mrbbbbbb");
     }
 }
